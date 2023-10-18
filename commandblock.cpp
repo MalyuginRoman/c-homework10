dd15ca3524f7fca39bec04e81271e852856edd56
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2023-10-18 08:31:23 +0500

    Create commandblock.cpp

---
+++ commandblock.cpp
@@ -0,0 +1,42 @@
+#include "async.h"
+
+void async::CommandBlock::notify(){
+    for(auto& i : subs){
+        i->update(status);
+    }
+    if(status == Status::stop) {
+        commands.clear();
+        if(dynamic) dynamic = false;
+    }
+}
+void async::CommandBlock::subscribe(std::unique_ptr<Observer> obs){
+    subs.push_back(std::move(obs));
+}
+void async::CommandBlock::setStatus(Status s){
+    status = s;
+    if(status == Status::start) commands.clear();
+    else if(status == Status::start_dynamic){
+        if(commands.size())
+            setStatus(Status::stop);
+        dynamic = true;
+        status = Status::start;
+    }
+    else if(status == Status::last_bulk){
+        if(!dynamic &&  commands.size() > 0) status = Status::stop;
+    }
+    notify();
+}
+void async::CommandBlock::appendCommnad(std::string & command){
+    if(!dynamic){
+        if(commands.size() == 0){
+            setStatus(Status::start);
+        }
+        commands.push_back(command);
+        if(commands.size() == (size_t)N){
+            setStatus(Status::stop);
+        }
+    }
+    else{
+        commands.push_back(command);
+    }
+}
